d16dca01e09cf21989513c1ad745959051e0a4be
chirag-varu <chiragvaru09@gmail.com>
2024-03-03 19:36:05 +0530

    Login Fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
 import React from "react";
 // import "./App.css";
 import { Login } from "./Component/Login-Component/Login";
-import { Profile } from "./Component/Profile-component/Profile";
-import { Aboutus } from "./Component/Aboutus-component/Aboutus";
-import { Booking } from "./Component/Booking-component/Booking";
-import { Gateway } from "./Component/Gateway-component/Gateway";
-import { Mainpage } from "./Component/Mainpage-Component/Mainpage";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -14,11 +9,6 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Login />} />
-          <Route path="/Profile" element={<Profile />} />
-          <Route path="/Mainpage" element={<Mainpage />} />
-          <Route path="/Booking" element={<Booking />} />
-          <Route path="/Gateway" element={<Gateway />} />
-          <Route path="/Aboutus" element={<Aboutus />} />
         </Routes>
       </Router>
     </div>

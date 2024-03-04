bafe454d68b4cabcc4856c6e824a6d627dc77046
chirag-varu <chiragvaru09@gmail.com>
2024-03-04 16:35:25 +0530

    Adding About-Us Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 // import "./App.css";
 import { Login } from "./Component/Login-Component/Login";
+import { Aboutus } from "./Component/AboutUs/Aboutus";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -9,6 +10,19 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Login />} />
+          {/* 
+            
+            for view this About-Us Component 
+            go to url and after localhost:3000 add /Aboutus
+            and this component will show up :)
+
+            this component is pushed becz the main component's Time picker is not ready yet
+            if it grt's ready i definetly push that till i will be pushing all other component's like this :)
+
+                                                                - Chirag();
+          
+          */}
+          <Route path="/Aboutus" element={<Aboutus />} /> 
         </Routes>
       </Router>
     </div>

52604e2a90c4f0abd639447bd9de73eac5c1ef5a
chirag-varu <chiragvaru09@gmail.com>
2024-03-04 16:39:55 +0530

    Adding About-Us Component

--- src/Component/Login-Component/Login.tsx
+++ src/Component/Login-Component/Login.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./login.css";
+import "./Login.css";
 import UsernameIcon from "./username_icon.png";
 import lock from "./password.png";
 
